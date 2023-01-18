8608601a7eadc1d607fa18a0eeb95e11c4b10f2d
miqueasledesma <mickeas98@gmail.com>
2023-01-18 18:46:03 -0300

    carousel test

--- src/components/SmCarousel/SmCarousel.jsx
+++ src/components/SmCarousel/SmCarousel.jsx
@@ -1,33 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { CardCarousel } from "../CardCarousel/CardCarousel";
 import { styleButton } from "../Carrousel/Carrousel";
 
 export const SmCarousel = ({ device }) => {
 
-    const [carruselImages, setCarruselImages] = useState()
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const handleNextClick = () => {
+        setCurrentIndex((currentIndex + 1) % components.length);
+    };
+
+    const handlePrevClick = () => {
+        setCurrentIndex((currentIndex - 1 + components.length) % components.length);
+    };
+
+    const toRender = (size) => {
+        let n = 3;
+        if (size === "Desktop") n = 3;
+        if (size === "Tablet-portrait") n = 2;
+        if (size === "Mobile-portrait" || size === "Mobile-landscape") n = 1;
+        let r = [];
+        for (let i = 0; i < n; i++) {
+            r.push(<CardCarousel />);
+        }
+        return r;
+    };
+
+    const components = [<>
+    <CardCarousel />
+    <h1>Soy Miqueas</h1>
+    </>, <h2>Que tal</h2>, <h3>Soy Miqueas</h3>];
 
 
-  const toRender = (size) => {
-    let n = 3;
-    if (size === "Desktop") n = 3;
-    if (size === "Tablet-portrait") n = 2;
-    if (size === "Mobile-portrait" || size === "Mobile-landscape") n = 1;
-    let r = [];
-    for (let i = 0; i < n; i++) {
-      r.push(<CardCarousel />);
-    }
-    return r;
-  };
-
-  return (
-    <div className="w-full h-fit flex justify-center items-center p-4">
-      <div className="bg-transparent flex justify-between flex-row mt-[15px] absolute w-full">
-        <button className={styleButton + " text-black"}>{"<"}</button>
-        <button className={styleButton + " text-black"}>{">"}</button>
-      </div>
-      <div className="w-[95%] flex gap-2 justify-between ">
-        {toRender(device)}
-      </div>
-    </div>
-  );
+    return (
+        <>
+            <div className="w-full h-fit flex justify-center items-center p-4">
+                <div className="bg-transparent flex justify-between flex-row mt-[15px] absolute w-full">
+                    <button className={styleButton + " text-black"} onClick={handleNextClick}>{"<"}</button>
+                    <button className={styleButton + " text-black"} onClick={handlePrevClick}>{">"}</button>
+                </div>
+                <div className="w-[95%] flex gap-2 justify-between ">
+                    {components[currentIndex]}
+                </div>
+            </div>
+        </>
+    );
 };
