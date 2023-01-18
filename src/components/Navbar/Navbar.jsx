af7d188119ba67d78a5f7f53774f2f6cf5aa7217
miqueasledesma <mickeas98@gmail.com>
2023-01-18 18:11:37 -0300

    navbar - responsive fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,26 +6,26 @@ import CartLogo from '../../assets/Cart-logo.svg';
 export const Navbar = () => {
     return (
         <nav className='bg-primary w-full h-20 flex justify-center items-center'>
-            <div className='w-5/6 flex justify-between items-center text-text2'>
-                <div className='flex justify-center items-center gap-8'>
+            <div className='w-[90%] flex justify-between items-center text-text2'>
+                <div className='flex justify-center items-center gap-8 -sm:w-full -sm:justify-center'>
                     <h2 className='text-4xl font-bold hover:cursor-pointer -sm:hidden'>TechStore</h2>
-                    <form className='flex w-fit '>
-                        <input type="text" className='rounded-l-lg h-10 w-80 text-text1 focus:border-none outline-none -sm:w-60 p-3' />
+                    <form className='flex w-fit -sm:w-full'>
+                        <input type="text" className='rounded-l-lg h-10 w-72 text-text1 focus:border-none outline-none -sm:w-60 p-3' />
                         <button type="submit" className='bg-secondary text-text2 rounded-r-lg px-5 -sm:px-3 hover:scale-105' >Search</button>
                     </form>
                 </div>
                 <div className='flex justify-center items-center gap-5 -sm:hidden'>
                     <div className='flex justify-center items-center hover:cursor-pointer hover:scale-105'>
-                        <img src={UserLogo} alt="loading..." className='w-8 h-8' />
-                        <h4>Cuenta</h4>
+                        <img src={UserLogo} alt="loading..." className='w-8 h-8 -sm:hidden' />
+                        <h4 className='-lg:hidden'>Cuenta</h4>
                     </div>
                     <div className='flex justify-center items-center gap-1 hover:cursor-pointer hover:scale-105'>
-                        <img src={HearthLogo} alt="loading..." className='w-6 h-6' />
-                        <h4>Favoritos</h4>
+                        <img src={HearthLogo} alt="loading..." className='w-6 h-6 -sm:hidden' />
+                        <h4 className='-lg:hidden'>Favoritos</h4>
                     </div>
                     <div className='flex justify-center items-center gap-1 hover:cursor-pointer hover:scale-105'>
-                        <img src={CartLogo} alt="loading..." className='w-6 h-6' />
-                        <h4>Carrito</h4>
+                        <img src={CartLogo} alt="loading..." className='w-6 h-6 -sm:hidden' />
+                        <h4 className='-lg:hidden'>Carrito</h4>
                     </div>
                 </div>
             </div>
