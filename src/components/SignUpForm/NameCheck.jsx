a0941c272e304db7ddf3acb2c1ea89d06df1cdfd
Mauricioam <mauricioam91@gmail.com>
2023-01-25 16:59:56 -0300

    termine signUp

--- src/components/SignUpForm/NameCheck.jsx
+++ src/components/SignUpForm/NameCheck.jsx
@@ -6,7 +6,8 @@ function NameCheck({ user,setUser, setOpenCheck , openCheck,setErrorMsg,errorMsg
  const { register, formState: { errors }, handleSubmit } = useForm();
 
  const captureCredentials = (nameData) =>{
-    setUser(nameData);
+   console.log(nameData.name)
+    setUser({...user,["name"]:nameData.name,["lastname"]:nameData.lastname});
     setOpenCheck({...openCheck,name:!openCheck.name})
  };
 
@@ -16,7 +17,7 @@ function NameCheck({ user,setUser, setOpenCheck , openCheck,setErrorMsg,errorMsg
         setErrorMsg({...errorMsg,[e.target.name]:errors})
      }
 };
- 
+ console.log()
 
   return (
     <form onSubmit={handleSubmit(captureCredentials)} onChange={handleInput} className='flex flex-col gap-3 my-3 '>
@@ -25,20 +26,33 @@ function NameCheck({ user,setUser, setOpenCheck , openCheck,setErrorMsg,errorMsg
            {...register("name", { 
             required: {
                value:true,
-               message:"Ingrese su mail"
-            }, 
+               message:"Ingrese su nombre"
+            },
+            pattern:{
+               value:/^[a-zA-Z]+$/g,
+               message:"Solo letras"
+            }
           })} 
            aria-invalid ={errors.name ? "true" : "false"}
            placeholder="Nombre"
             className='p-2 border rounded-md'
          />
     <input
-           {...register("lastname", { required: true })} 
-           aria-invalid ={errors.name ? "true" : "false"}
+           {...register("lastname", { 
+            required: {
+               value:true,
+               message:"Ingrese su apellido"
+            },
+            pattern:{
+               value:/^[a-zA-Z]+$/g,
+               message:"Solo letras"
+            }  })} 
+           aria-invalid ={errors.lastname ? "true" : "false"}
            placeholder="Apellido"
             className='p-2 border rounded-md'
          />
-         
+           {errors.name?.message && <p className="text-error text-sm">{errors.name.message}</p>}
+           {errors.lastname?.message && <p className="text-error text-sm">{errors.lastname.message}</p>}
     <button type='submit' className='bg-secondary rounded-lg lg:p-2 xs:p-4 w-32 '>Guardar</button>
     </form>
   )
