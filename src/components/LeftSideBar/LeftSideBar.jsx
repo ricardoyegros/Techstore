c252f437ef7ce0accd7bdb756bdf8f7a3a99d841
Ricardo Yegros <ricardo_yegros@hotmail.com>
2023-01-29 22:11:39 -0300

    logic added to filters in responsive mode

--- src/components/LeftSideBar/LeftSideBar.jsx
+++ src/components/LeftSideBar/LeftSideBar.jsx
@@ -1,72 +1,86 @@
 import React from "react";
-import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllProducts } from "../../redux/actions/getAllProducts";
 
-export const LeftSideBar = ({filter, setFilter}) => {
+export const LeftSideBar = ({ filter, setFilter, handleCheck }) => {
   const dispatch = useDispatch();
   const categorys = useSelector(
     (state) => state.getAllCategorysReducer.allCategorys
   );
   const brands = useSelector((state) => state.getAllBrandsReducer.allBrands);
-
-
-
-  useEffect(() => {
-    dispatch(getAllProducts(filter));
-  }, [filter]);
-
-  const handleCheck = e => {
-    e.preventDefault();
-    setFilter({
-      ...filter,
-      [e.target.name]: e.target.value,
-      page: 1
-    });
-  };
-
-
-
   return (
     <div className="flex flex-col gap-2 justify-center items-center w-[30%] h-fit -lg:hidden">
       <div className="flex w-[80%] justify-between h-fit py-2 text-primary">
         <h4 className="select-none font-extrabold">Categorias</h4>
       </div>
       <div className="h-1 w-[80%] bg-primary"></div>
-
-      <div className="flex w-[80%] flex-col gap-4 justify-between h-fit py-2 text-primary" >
+      <div className="flex w-[80%] flex-col gap-4 justify-between h-fit py-2 text-primary">
         {categorys &&
-          categorys.map(category => (
+          categorys.map((category) => (
             <div className="flex w-full items-center justify-center">
               <div className="flex w-full justify-between items-center gap-4">
-                <button value={category.id} name='categoryId' onClick={filter.categoryId == category.id ? null : handleCheck} className={filter.categoryId == category.id ? 'hover:cursor-not-allowed' : "hover:cursor-pointer"}>{category.name}</button>
-                {
-                  filter.categoryId == category.id ?
-                    <button name='categoryId' value='' onClick={handleCheck} className={filter.categoryId == category.id ? 'visible' : 'hidden'}>X</button> :
-                    null
-                }
+                <button
+                  value={category.id}
+                  name="categoryId"
+                  onClick={
+                    filter.categoryId == category.id ? null : handleCheck
+                  }
+                  className={
+                    filter.categoryId == category.id
+                      ? "hover:cursor-not-allowed"
+                      : "hover:cursor-pointer"
+                  }
+                >
+                  {category.name}
+                </button>
+                {filter.categoryId == category.id ? (
+                  <button
+                    name="categoryId"
+                    value=""
+                    onClick={handleCheck}
+                    className={
+                      filter.categoryId == category.id ? "visible" : "hidden"
+                    }
+                  >
+                    X
+                  </button>
+                ) : null}
               </div>
             </div>
           ))}
       </div>
-
       <div className="flex w-[80%] justify-between h-fit py-2 text-primary">
         <h4 className="font-extrabold select-none">Marcas</h4>
       </div>
-
       <div className="h-1 w-[80%] bg-primary"></div>
-
       <div className="flex w-[80%] flex-col gap-4 justify-between h-fit text-primary">
-      {brands &&
-          brands.map(brand => (
+        {brands &&
+          brands.map((brand) => (
             <div className="flex w-full items-center justify-center">
               <div className="flex w-full justify-between items-center gap-4">
-                <button value={brand.id} name='brandId' onClick={filter.brandId == brand.id ? null : handleCheck} className={filter.brandId == brand.id ? 'hover:cursor-not-allowed' : "hover:cursor-pointer"}>{brand.name}</button>
-                {
-                  filter.brandId == brand.id ?
-                    <button name='brandId' value='' onClick={handleCheck} className={filter.brandId == brand.id ? 'visible' : 'hidden'}>X</button> :
-                    null
-                }
+                <button
+                  value={brand.id}
+                  name="brandId"
+                  onClick={filter.brandId == brand.id ? null : handleCheck}
+                  className={
+                    filter.brandId == brand.id
+                      ? "hover:cursor-not-allowed"
+                      : "hover:cursor-pointer"
+                  }
+                >
+                  {brand.name}
+                </button>
+                {filter.brandId == brand.id ? (
+                  <button
+                    name="brandId"
+                    value=""
+                    onClick={handleCheck}
+                    className={
+                      filter.brandId == brand.id ? "visible" : "hidden"
+                    }
+                  >
+                    X
+                  </button>
+                ) : null}
               </div>
             </div>
           ))}
--- src/components/RightSidebar/RightSideBar.jsx
+++ src/components/RightSidebar/RightSideBar.jsx
@@ -2,23 +2,35 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { MainCard } from '../MainCard/MainCard';
 import { Pagination } from '../Pagination/Pagination';
-export const RightSideBar = ({ filter, setFilter }) => {
+export const RightSideBar = ({ filter, setFilter, handleCheck }) => {
   let allProducts = useSelector((state) => state.getAllProductsReducer.allProducts);
-  console.log(allProducts);
   const categorys = useSelector((state) => state.getAllCategorysReducer.allCategorys);
   const brands = useSelector((state) => state.getAllBrandsReducer.allBrands);
   const classForSelects = 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-3 w-60 ';
+  function handleChange(e){
+    const {name, value} = e.target;
+    if(Number(value) !== NaN){
+      setFilter({
+        ...filter,
+        [name]: value
+      })
+    }
+  };
   return (
     <div className='flex-col w-[70%] h-full  '>
       <div className='lg:hidden flex gap-5 justify-center m-5'>
-        <select className={classForSelects}>
+        <form>
+        <select onChange={handleChange} name='categoryId' className={classForSelects}>
           <option selected disabled>Categorias</option>
-          {categorys && categorys.map((category) => (<option key={category.id}>{category.name}</option>))}
+          {categorys && categorys.map((category) => (<option value={category.id}  key={category.id}>{category.name}</option>))}
         </select>
-        <select className={classForSelects}>
-          <option selected disabled>Marcas</option>
-          {brands && brands.map((brand) => (<option key={brand.id}>{brand.name}</option>))}
+        </form>
+        <form>
+        <select onChange={handleChange} name='brandId' className={classForSelects}>
+          <option  selected disabled>Marcas</option>
+          {brands && brands.map((brand) => (<option value={brand.id} key={brand.id}>{brand.name}</option>))}
         </select>
+        </form>
       </div>
       <div className='flex flex-row justify-center flex-wrap gap-8'>
         {allProducts.content && (!allProducts.content[0] ? <h2>No se encontraron los productos</h2> : allProducts.content?.slice(0, 12).map((product) => (<MainCard {...product} />)))}
