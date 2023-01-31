12da99512998619014d7e1a4e6eea2da919433b9
Mauricioam <mauricioam91@gmail.com>
2023-01-31 18:02:39 -0300

    added forgotPassword component

--- src/components/SignInForm/ForgetPassword.jsx
+++ src/components/SignInForm/ForgetPassword.jsx
@@ -1,14 +1,22 @@
 import { yupResolver } from "@hookform/resolvers/yup";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { forgotPassword } from "../../redux/actions/UserActions/UserActions";
 import { signInSchema } from "../../schemas/signIn.schema";
 import { fieldFormUtils } from "../../utils/fieldForm.utils";
 import { inputField } from "../../utils/inputField.utils";
+import { propsModal } from "../../utils/propsModal";
 import Layout from "../Layout/Layout";
+import Modal from "../Modal/Modal";
+import ModalError from "../Modal/ModalError";
 
 function ForgetPassword() {
+
+  const dispatch = useDispatch();
   /* State modal */
   const [openModal, setOpenModal] = useState(false);
+  const [error, setError] = useState(false);
   /* State user */
   const [email, setEmail] = useState();
 
@@ -27,23 +35,30 @@ function ForgetPassword() {
     setOpenModal
   );
 
-
-
-
+const dispatchEmail = () => {
+  if(email && email.length > 0){
+    dispatch(forgotPassword(email, openModal, setOpenModal, error, setError))
+  }
+};
+console.log(email);
   return (
+    <>
+    {error && <ModalError props={propsModal("Error","Revisa tus datos ingresados","Cerrar",setError)}/>}
+    {openModal && <Modal props={propsModal("Se ha enviado un mail a tu casilla","","Volver al inicio",setOpenModal)} />} 
     <Layout>
       <div className="flex justify-center py-12">
         <form onSubmit={handleSubmit(saveMessage)}  className="border rounded-lg lg:w-2/5 md:w-2/3 xs:w-1/2">
           <div className="flex flex-col gap-9 w-full lg:p-16 md:p-12 sm:p-8 xs:p-4">
             <label>Ingrese su email</label>
             {inputField("email", register, handleFormChange, errors)}
-            <button  className="bg-secondary rounded-lg lg:p-2 xs:p-4">
+            <button onClick={dispatchEmail}   className="bg-secondary rounded-lg lg:p-2 xs:p-4">
               Recuperar contraseña
             </button>
           </div>
         </form>
       </div>
     </Layout>
+    </>
   );
 }
 
