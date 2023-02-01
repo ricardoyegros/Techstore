200e0b9310f844a1502b5d14230f47cf7068efd1
Ricardo Yegros <ricardo_yegros@hotmail.com>
2023-02-01 16:01:01 -0300

    feat styles to user dashboard

--- src/components/UserDashboard/UserDashoardMenu.jsx
+++ src/components/UserDashboard/UserDashoardMenu.jsx
@@ -1,28 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import  UserLogo  from "../../assets/User-logo.svg"
-import {
-  PrivateRoutes,
-  PrivateRoutesDash,
-} from "../../routes/privates-routes/private-user-routes";
-
+import { MenuOptions } from "./menuDashboard";
+import {BsArrowLeftShort} from "react-icons/bs"
 function UserDashoardMenu({ children }) {
+  const [open, setOpen] = useState(true);
+  const classForLiButtons = `flex rounded-md items-center py-3 ${open ? "pl-8" : "pl-4" } w-full hover:font-bold hover:text-primary hover:cursor-pointer hover:bg-sky`
   return (
     <div className="flex">
-      <div className="w-1/12 h-full bg-gray2">
-        <ul className=" flex flex-col h-[80vh]    my-3 w-full">
-          <span className="flex justify-center  p-3  w-full   hover:bg-gray3">
-            <Link to={`/${PrivateRoutes.USER}/${PrivateRoutes.DASHBOARD}`}>
-              Cuenta
-            </Link>
-          </span>
-          <span className="flex justify-center  p-3 w-full   hover:bg-gray3">
-            <Link
-              to={`/${PrivateRoutes.USER}/${PrivateRoutes.DASHBOARD}/${PrivateRoutesDash.ORDERS}`}
-            >
-              Ordenes
-            </Link>
-          </span>
+      <div className={`${open ? "w-48" : "w-16" } h-screen bg-primary border-t-2 border-white text-white duration-300 relative`}>
+        <BsArrowLeftShort onClick={()=>setOpen(!open)} className={`bg-white text-primary text-3xl rounded-full ${!open && "rotate-[180deg]" } duration-500 absolute border border-primary hover:cursor-pointer -top-2 -right-3` }/>
+        <ul className=" flex flex-col h-[80vh] my-3 w-full">
+          {MenuOptions &&
+            MenuOptions.map((item) => (
+              <li key={item.buttonName} className={classForLiButtons}>
+                <Link to={item.Links}>
+                  {item.icon}
+                  <span className={`${!open && "hidden"}`}>{item.buttonName}</span>
+                </Link>
+              </li>
+            ))}
         </ul>
       </div>
       {children}
